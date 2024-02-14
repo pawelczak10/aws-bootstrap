a5d02c3bad2ab29d37f1b06b0299549451d4a0a6
Your Name <you@example.com>
2024-02-14 19:06:41 +0000

    Create infrastructure via CloudFormation

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -2,7 +2,7 @@ source aws_credentials.sh
 
 STACK_NAME=awsbootstrap
 REGION=us-east-1 
-CLI_PROFILE=Pawel
+CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t2.micro 
 
@@ -20,6 +20,6 @@ aws cloudformation deploy \
     # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
-    --profile Pawel \
+    --profile awsbootstrap \
     --query "Exports[?Name=='InstanceEndpoint'].Value" 
 fi
